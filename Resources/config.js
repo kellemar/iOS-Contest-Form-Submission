a674f399787ad687cb9f9633b12a9b4a6c7be870
Alvin De Cruz <decruz@gmail.com>
2011-06-14 04:59:15 +0800

    Initial prep for Github

--- Resources/config.js
+++ Resources/config.js
@@ -1,9 +1,9 @@
-Titanium.App.Properties.setString("bannerListURL","http://epicureasia.com/banners/banner_list.php");
-Titanium.App.Properties.setString("newsListURL","http://epicureasia.com/promotions-contests2.php");
-Titanium.App.Properties.setString("galleryListURL","http://epicureasia.com/food_gallery2.php");
-Titanium.App.Properties.setString("submissionURL","http://epicureasia.com/photoprocess2.php");
+Titanium.App.Properties.setString("bannerListURL","");
+Titanium.App.Properties.setString("newsListURL","");
+Titanium.App.Properties.setString("galleryListURL","");
+Titanium.App.Properties.setString("submissionURL","");
 
-Titanium.App.Properties.setString("subscriptionURL","http://epicureasia.com/about/subscription/");
-Titanium.App.Properties.setString("subscriptionTitle","epicure Subscription");
+Titanium.App.Properties.setString("subscriptionURL","");
+Titanium.App.Properties.setString("subscriptionTitle","");
 
-Titanium.App.Properties.setString("newsImageURL","http://epicureasia.com/wp-content");
+Titanium.App.Properties.setString("newsImageURL","");
